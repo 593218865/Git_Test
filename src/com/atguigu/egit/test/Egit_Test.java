8537441068e39f4c3fcc8c705243abab2b2e04ca
593218865 <593218865@qq.com>
2019-09-03 11:58:36 +0800

    Update Egit_Test.java

--- src/com/atguigu/egit/test/Egit_Test.java
+++ src/com/atguigu/egit/test/Egit_Test.java
@@ -4,5 +4,6 @@ public class Egit_Test {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaaaa");
 		System.out.println("bbbbbbbbbb");
+		System.out.println("cccccccccc");
 	}
 }
